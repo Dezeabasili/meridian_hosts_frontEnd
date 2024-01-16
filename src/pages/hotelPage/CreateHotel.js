b11d5ea8f8b544d37d54a380b9f756c9ca77379a
Dezeabasili <ezeabasili@yahoo.co.uk>
2024-01-15 20:06:58 -0600

    Updated CreateHotel.js

--- src/pages/hotelPage/CreateHotel.js
+++ src/pages/hotelPage/CreateHotel.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import useAxiosInterceptors from "../../hooks/useAxiosWithInterceptors";
 import { baseURL } from "../../context/authContext";
+import { RotatingLines } from "react-loader-spinner";
 
 const CreateHotel = () => {
   const [name, setName] = useState();
@@ -15,11 +16,53 @@ const CreateHotel = () => {
   const [distanceToClosestTouristLocation, setDistanceToClosestTouristLocation] = useState();
   const [manager, setManager] = useState();
   const [staff, setStaff] = useState();
+  const [cityData, setCityData] = useState();
+  const [cityDataFocus, setCityDataFocus] = useState(false);
+  const [hotelTypeData, setHotelTypeData] = useState();
+  const [hotelTypeDataFocus, setHotelTypeDataFocus] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const axiosWithInterceptors = useAxiosInterceptors();
   const navigate = useNavigate();
+  const runOnce = useRef(false);
+
+  const errorDiv = error ? <div className="error">{error}</div> : "";
+
+  useEffect(() => {
+    if (runOnce.current === false) {
+      const references = async () => {
+        setLoading(true);
+        setError(null);
+        try {
+          const resp = await axiosWithInterceptors.get(baseURL + "api/v1/hotels/allcityrefs");
+          // console.log("hotels: ", resp.data.data);
+          setCityData([...resp.data.data]);
+
+          const resp2 = await axiosWithInterceptors.get(
+            baseURL + "api/v1/hotels/allhoteltyperefs"
+          );
+          // console.log("hotels: ", resp.data.data);
+          setHotelTypeData([...resp2.data.data]);
+
+          setLoading(false);
+        } catch (err) {
+          console.log(err.message);
+          setError(err.response.data.message);
+        }
+      };
+
+      references();
+    }
+
+    return () => {
+      runOnce.current = true;
+    };
+  }, []);
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setError(null);
     try {
       const givenCoordinates = coordinates.split(",");
       let coordinatesToNumbers = [];
@@ -54,12 +97,29 @@ const CreateHotel = () => {
       navigate("/hotels");
     } catch (err) {
       console.log(err);
+      setError(err.response.data.message);
     }
   };
 
   return (
     <div className="register">
-      <form className="registerContainer" onSubmit={handleSubmit}>
+      <>
+        {loading && (
+          <RotatingLines
+            visible={true}
+            height="96"
+            width="96"
+            color="grey"
+            strokeWidth="5"
+            animationDuration="0.75"
+            ariaLabel="rotating-lines-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+        )}
+      </>
+      <>
+          {!loading && <form className="registerContainer" onSubmit={handleSubmit}>
         <h3 className="registerTitle">Provide hotel details</h3>
 
         <div className="registerDiv">
@@ -73,24 +133,58 @@ const CreateHotel = () => {
           />
         </div>
         <div className="registerDiv">
-          <label htmlFor="hotelCity">Hotel city:</label>
+          <label htmlFor="hotelCity">Hotel city reference number:</label>
           <input
             id="hotelCity"
             type="text"
             value={city || ''}
             onChange={(e) => setCity(e.target.value)}
             autoComplete="off"
+            onFocus={() => setCityDataFocus(true)}
+            onBlur={() => setCityDataFocus(false)}
           />
+          <div
+            className={
+              cityDataFocus ? "showInstructions" : "hideInstructions"
+            }
+          >
+            {cityData?.map((city) => (
+              <div key={city._id}>
+                <p style={{ textTransform: "capitalize" }}>
+                  City Name: {city.cityName}
+                </p>
+                <p>City Reference: {city._id}</p>
+                <br />
+              </div>
+            ))}
+          </div>
         </div>
         <div className="registerDiv">
-          <label htmlFor="hotelType">Hotel type:</label>
+          <label htmlFor="hotelType">Hotel type reference number:</label>
           <input
             id="hotelType"
             type="text"
             value={type || ''}
             onChange={(e) => setType(e.target.value)}
             autoComplete="off"
+            onFocus={() => setHotelTypeDataFocus(true)}
+            onBlur={() => setHotelTypeDataFocus(false)}
           />
+          <div
+            className={
+              hotelTypeDataFocus ? "showInstructions" : "hideInstructions"
+            }
+          >
+            {hotelTypeData?.map((hotelType) => (
+              <div key={hotelType._id}>
+                <p style={{ textTransform: "capitalize" }}>
+                  Hotel Type: {hotelType.hotelType}
+                </p>
+                <p>Hotel Type Reference: {hotelType._id}</p>
+                <br />
+              </div>
+            ))}
+          </div>
         </div>
 
         <div className="registerDiv">
@@ -195,7 +289,12 @@ const CreateHotel = () => {
         >
           Continue
         </button>
-      </form>
+      </form>}
+      </>
+      <>
+      {error && errorDiv}
+      </>
+      
     </div>
   );
 };
--- src/pages/hotelPage/UpdateHotel.js
+++ src/pages/hotelPage/UpdateHotel.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import useAxiosInterceptors from "../../hooks/useAxiosWithInterceptors";
 import { baseURL } from "../../context/authContext";
+import { RotatingLines } from "react-loader-spinner";
 
 const UpdateHotel = () => {
   const [name, setName] = useState();
@@ -12,9 +13,49 @@ const UpdateHotel = () => {
   const [manager, setManager] = useState();
   const [addStaff, setAddStaff] = useState();
   const [removeStaff, setRemoveStaff] = useState();
+  const [cityData, setCityData] = useState();
+  const [cityDataFocus, setCityDataFocus] = useState(false);
+  const [hotelTypeData, setHotelTypeData] = useState();
+  const [hotelTypeDataFocus, setHotelTypeDataFocus] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const axiosWithInterceptors = useAxiosInterceptors();
   const navigate = useNavigate();
   const location = useLocation()
+  const runOnce = useRef(false);
+
+  const errorDiv = error ? <div className="error">{error}</div> : "";
+
+  useEffect(() => {
+    if (runOnce.current === false) {
+      const references = async () => {
+        setLoading(true);
+        setError(null);
+        try {
+          const resp = await axiosWithInterceptors.get(baseURL + "api/v1/hotels/allcityrefs");
+          // console.log("hotels: ", resp.data.data);
+          setCityData([...resp.data.data]);
+
+          const resp2 = await axiosWithInterceptors.get(
+            baseURL + "api/v1/hotels/allhoteltyperefs"
+          );
+          // console.log("hotels: ", resp.data.data);
+          setHotelTypeData([...resp2.data.data]);
+
+          setLoading(false);
+        } catch (err) {
+          console.log(err.message);
+          setError(err.response.data.message);
+        }
+      };
+
+      references();
+    }
+
+    return () => {
+      runOnce.current = true;
+    };
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -33,7 +74,23 @@ const UpdateHotel = () => {
 
   return (
     <div className="register">
-      <form className="registerContainer" onSubmit={handleSubmit}>
+      <>
+        {loading && (
+          <RotatingLines
+            visible={true}
+            height="96"
+            width="96"
+            color="grey"
+            strokeWidth="5"
+            animationDuration="0.75"
+            ariaLabel="rotating-lines-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+        )}
+      </>
+      <>
+          {!loading && <form className="registerContainer" onSubmit={handleSubmit}>
         <h3 className="registerTitle">
           Provide only the hotel information to change 
         </h3>
@@ -49,24 +106,58 @@ const UpdateHotel = () => {
           />
         </div>
         <div className="registerDiv">
-          <label htmlFor="hotelCity">Hotel city:</label>
+          <label htmlFor="hotelCity">Hotel city reference number:</label>
           <input
             id="hotelCity"
             type="text"
-            value={city}
+            value={city || ''}
             onChange={(e) => setCity(e.target.value)}
             autoComplete="off"
+            onFocus={() => setCityDataFocus(true)}
+            onBlur={() => setCityDataFocus(false)}
           />
+          <div
+            className={
+              cityDataFocus ? "showInstructions" : "hideInstructions"
+            }
+          >
+            {cityData?.map((city) => (
+              <div key={city._id}>
+                <p style={{ textTransform: "capitalize" }}>
+                  City Name: {city.cityName}
+                </p>
+                <p>City Reference: {city._id}</p>
+                <br />
+              </div>
+            ))}
+          </div>
         </div>
         <div className="registerDiv">
-          <label htmlFor="hotelType">Hotel type:</label>
+          <label htmlFor="hotelType">Hotel type reference number:</label>
           <input
             id="hotelType"
             type="text"
-            value={type}
+            value={type || ''}
             onChange={(e) => setType(e.target.value)}
             autoComplete="off"
+            onFocus={() => setHotelTypeDataFocus(true)}
+            onBlur={() => setHotelTypeDataFocus(false)}
           />
+          <div
+            className={
+              hotelTypeDataFocus ? "showInstructions" : "hideInstructions"
+            }
+          >
+            {hotelTypeData?.map((hotelType) => (
+              <div key={hotelType._id}>
+                <p style={{ textTransform: "capitalize" }}>
+                  Hotel Type: {hotelType.hotelType}
+                </p>
+                <p>Hotel Type Reference: {hotelType._id}</p>
+                <br />
+              </div>
+            ))}
+          </div>
         </div>
         <div className="registerDiv">
           <label htmlFor="hotelAddress">Hotel address:</label>
@@ -124,7 +215,12 @@ const UpdateHotel = () => {
         <button className="signUpButton" disabled={!name && !city && !type && !address && !description && !manager && !addStaff && !removeStaff}>
           Continue
         </button>
-      </form>
+      </form>}
+      </>
+      <>
+      {error && errorDiv}
+      </>
+      
     </div>
   );
 };
