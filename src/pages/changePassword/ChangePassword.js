adb3aa2d0b5526b620db6ea2e21602070d5f5237
Dezeabasili <ezeabasili@yahoo.co.uk>
2024-01-21 15:06:05 -0600

    Updated CreateHotel.js

--- src/pages/changePassword/ChangePassword.js
+++ src/pages/changePassword/ChangePassword.js
@@ -1,99 +1,153 @@
-import './changePassword.css'
-import { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import useAxiosInterceptors from '../../hooks/useAxiosWithInterceptors'
+import "./changePassword.css";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useAxiosInterceptors from "../../hooks/useAxiosWithInterceptors";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 import { baseURL } from "../../context/authContext";
 
-const password_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
+const password_REGEX =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
 const ChangePassword = () => {
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [password, setPassword] = useState("");
+  const [validPassword, setValidPassword] = useState();
+  const [passwordFocus, setPasswordFocus] = useState();
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [validConfirmPassword, setValidConfirmPassword] = useState();
+  const [confirmPasswordFocus, setConfirmPasswordFocus] = useState();
+  const [togglePassword, setTogglePassword] = useState(false);
+  const [togglePassword2, setTogglePassword2] = useState(false);
+  const [togglePassword3, setTogglePassword3] = useState(false);
+  const navigate = useNavigate();
+  const axiosWithInterceptors = useAxiosInterceptors();
 
-    const [currentPassword, setCurrentPassword] = useState('')
-    const [password, setPassword] = useState('')
-    const [validPassword, setValidPassword] = useState()
-    const [passwordFocus, setPasswordFocus] = useState()
-    const [confirmPassword, setConfirmPassword] = useState('')
-    const [validConfirmPassword, setValidConfirmPassword] = useState()
-    const [confirmPasswordFocus, setConfirmPasswordFocus] = useState()
-    const navigate = useNavigate()
-    const axiosWithInterceptors = useAxiosInterceptors()
+  useEffect(() => {
+    setValidPassword(password_REGEX.test(password));
+    setValidConfirmPassword(password === confirmPassword);
+  }, [password, confirmPassword]);
 
-    useEffect(() => {
-        setValidPassword(password_REGEX.test(password))
-        setValidConfirmPassword(password === confirmPassword)
-    }, [password, confirmPassword])
-
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        try {
-          
-            const response = await axiosWithInterceptors.post(baseURL + 'api/v1/auth/changepassword', {currentPassword, password })
-            // console.log(response.data)
-            setPassword('')
-            setConfirmPassword('')
-            navigate('/login')
-
-        } catch (err) {
-            if (!err?.response)
-                console.log('no server response')
-            else console.log(err)
-        }
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axiosWithInterceptors.post(
+        baseURL + "api/v1/auth/changepassword",
+        { currentPassword, password }
+      );
+      // console.log(response.data)
+      setPassword("");
+      setConfirmPassword("");
+      navigate("/login");
+    } catch (err) {
+      if (!err?.response) console.log("no server response");
+      else console.log(err);
     }
-    return (
-        <div className='register'>
-            <form className='registerContainer' onSubmit={handleSubmit}>
-                <h1 className='registerTitle'>Reset Password</h1>
-               
-                <div className='registerDiv'>
-                    <label htmlFor='password'>Current password:</label>
-                    <input
-                        id='password'
-                        type='password'
-                        value={currentPassword}
-                        onChange={(e) => setCurrentPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className='registerDiv'>
-                    <label htmlFor='password'>New password:</label>
-                    <input
-                        id='password'
-                        type='password'
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                        onFocus={() => setPasswordFocus(true)}
-                        onBlur={() => setPasswordFocus(false)}
-                    />
-                    <p className={passwordFocus && !validPassword ? 'showInstructions' : 'hideInstructions'}>
-                    Password must contain 8 - 24 characters. <br/>
-                    It must include the following: <br/>
-                    a lowercase letter, an uppercase letter, a number, one of these special characters % ! @ # $
-                    </p>
-                </div>
-                <div className='registerDiv'>
-                    <label htmlFor='confirmPwd'>Confirm new password:</label>
-                    <input
-                        id='confirmPwd'
-                        type='password'
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                        required
-                        onFocus={() => setConfirmPasswordFocus(true)}
-                        onBlur={() => setConfirmPasswordFocus(false)}
-                    />
-                    <p className={confirmPasswordFocus && !validConfirmPassword ? 'showInstructions' : 'hideInstructions'}>
-                    Passwords must match.
-                    </p>
-                </div>
+  };
 
-                <button className='signUpButton' disabled={!currentPassword || !validPassword || !validConfirmPassword} >Submit new password</button>
+  const tryAbraham = () => {
+    setTogglePassword((prev) => !prev);
+  };
 
+  const tryAbraham2 = () => {
+    setTogglePassword2((prev) => !prev);
+  };
 
-            </form>
+  const tryAbraham3 = () => {
+    setTogglePassword3((prev) => !prev);
+  };
+
+  return (
+    <div className="register">
+      <form className="registerContainer" onSubmit={handleSubmit}>
+        <h1 className="registerTitle">Reset Password</h1>
+
+        <div className="registerDiv">
+          <label htmlFor="password">Current password:</label>
+          <div className="abraham">
+            <input
+              id="password"
+              type={!togglePassword ? "password" : "text"}
+              value={currentPassword}
+              onChange={(e) => setCurrentPassword(e.target.value)}
+              required
+            />
+            <FontAwesomeIcon
+              icon={faEye}
+              className="headerSearchIcon headerSearchIcon1"
+              onClick={tryAbraham}
+            />
+          </div>
         </div>
-    )
-}
+        <div className="registerDiv">
+          <label htmlFor="password">New password:</label>
+          <div className="abraham">
+            <input
+              id="password"
+              type={!togglePassword2 ? "password" : "text"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              onFocus={() => setPasswordFocus(true)}
+              onBlur={() => setPasswordFocus(false)}
+            />
+            <FontAwesomeIcon
+              icon={faEye}
+              className="headerSearchIcon headerSearchIcon1"
+              onClick={tryAbraham2}
+            />
+          </div>
+          <p
+            className={
+              passwordFocus && !validPassword
+                ? "showInstructions"
+                : "hideInstructions"
+            }
+          >
+            Password must contain 8 - 24 characters. <br />
+            It must include the following: <br />a lowercase letter, an
+            uppercase letter, a number, one of these special characters % ! @ #
+            $
+          </p>
+        </div>
+        <div className="registerDiv">
+          <label htmlFor="confirmPwd">Confirm new password:</label>
+          <div className="abraham">
+            <input
+              id="confirmPwd"
+              type={!togglePassword3 ? "password" : "text"}
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+              onFocus={() => setConfirmPasswordFocus(true)}
+              onBlur={() => setConfirmPasswordFocus(false)}
+            />
+            <FontAwesomeIcon
+              icon={faEye}
+              className="headerSearchIcon headerSearchIcon1"
+              onClick={tryAbraham3}
+            />
+          </div>
+          <p
+            className={
+              confirmPasswordFocus && !validConfirmPassword
+                ? "showInstructions"
+                : "hideInstructions"
+            }
+          >
+            Passwords must match.
+          </p>
+        </div>
 
-export default ChangePassword
+        <button
+          className="signUpButton"
+          disabled={!currentPassword || !validPassword || !validConfirmPassword}
+        >
+          Submit new password
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ChangePassword;
--- src/pages/passwordReset/PasswordReset.js
+++ src/pages/passwordReset/PasswordReset.js
@@ -1,85 +1,130 @@
-import { useState, useEffect } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 import { baseURL } from "../../context/authContext";
 
-const password_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
+const password_REGEX =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
 const PasswordReset = () => {
-    const [password, setPassword] = useState('')
-    const [validPassword, setValidPassword] = useState()
-    const [passwordFocus, setPasswordFocus] = useState()
-    const [confirmPassword, setConfirmPassword] = useState('')
-    const [validConfirmPassword, setValidConfirmPassword] = useState()
-    const [confirmPasswordFocus, setConfirmPasswordFocus] = useState()
-    const navigate = useNavigate()
-    const {resettoken, user_id} = useParams()
+  const [password, setPassword] = useState("");
+  const [validPassword, setValidPassword] = useState();
+  const [passwordFocus, setPasswordFocus] = useState();
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [validConfirmPassword, setValidConfirmPassword] = useState();
+  const [confirmPasswordFocus, setConfirmPasswordFocus] = useState();
+  const [togglePassword, setTogglePassword] = useState(false);
+  const [togglePassword2, setTogglePassword2] = useState(false);
+  const navigate = useNavigate();
+  const { resettoken, user_id } = useParams();
 
-    useEffect(() => {
-        setValidPassword(password_REGEX.test(password))
-        setValidConfirmPassword(password === confirmPassword)
-    }, [password, confirmPassword])
+  useEffect(() => {
+    setValidPassword(password_REGEX.test(password));
+    setValidConfirmPassword(password === confirmPassword);
+  }, [password, confirmPassword]);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        try {
-            const response = await axios.post(baseURL + `api/v1/auth/resetpassword/${resettoken}/${user_id}`, { password })
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        baseURL + `api/v1/auth/resetpassword/${resettoken}/${user_id}`,
+        { password }
+      );
 
-            // console.log(response.data)
-            setPassword('')
-            setConfirmPassword('')
-            navigate('/login')
-
-        } catch (err) {
-            if (!err?.response)
-                console.log('no server response')
-            else console.log(err)
-        }
-
+      // console.log(response.data)
+      setPassword("");
+      setConfirmPassword("");
+      navigate("/login");
+    } catch (err) {
+      if (!err?.response) console.log("no server response");
+      else console.log(err);
     }
-    return (
-        <div className='register'>
-            <form className='registerContainer' onSubmit={handleSubmit}>
-                <h1 className='registerTitle'>Reset Password</h1>
-               
-                <div className='registerDiv'>
-                    <label htmlFor='password'>Password:</label>
-                    <input
-                        id='password'
-                        type='password'
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                        onFocus={() => setPasswordFocus(true)}
-                        onBlur={() => setPasswordFocus(false)}
-                    />
-                    <p className={passwordFocus && !validPassword ? 'showInstructions' : 'hideInstructions'}>
-                    Password must contain 8 - 24 characters. <br/>
-                    It must include the following: <br/>
-                    a lowercase letter, an uppercase letter, a number, one of these special characters % ! @ # $ 
-                    </p>
-                </div>
-                <div className='registerDiv'>
-                    <label htmlFor='confirmPwd'>Confirm Password:</label>
-                    <input
-                        id='confirmPwd'
-                        type='password'
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                        required
-                        onFocus={() => setConfirmPasswordFocus(true)}
-                        onBlur={() => setConfirmPasswordFocus(false)}
-                    />
-                    <p className={confirmPasswordFocus && !validConfirmPassword ? 'showInstructions' : 'hideInstructions'}>
-                    Passwords must match.
-                    </p>
-                </div>
+  };
 
-                <button className='signUpButton' disabled={!validPassword || !validConfirmPassword} >Submit new password</button>
+  const tryAbraham = () => {
+    setTogglePassword((prev) => !prev);
+  };
 
-            </form>
+  const tryAbraham2 = () => {
+    setTogglePassword2((prev) => !prev);
+  };
+
+  return (
+    <div className="register">
+      <form className="registerContainer" onSubmit={handleSubmit}>
+        <h1 className="registerTitle">Reset Password</h1>
+
+        <div className="registerDiv">
+          <label htmlFor="password">Password:</label>
+          <div className="abraham">
+            <input
+              id="password"
+              type={!togglePassword ? "password" : "text"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              onFocus={() => setPasswordFocus(true)}
+              onBlur={() => setPasswordFocus(false)}
+            />
+            <FontAwesomeIcon
+              icon={faEye}
+              className="headerSearchIcon headerSearchIcon1"
+              onClick={tryAbraham}
+            />
+          </div>
+          <p
+            className={
+              passwordFocus && !validPassword
+                ? "showInstructions"
+                : "hideInstructions"
+            }
+          >
+            Password must contain 8 - 24 characters. <br />
+            It must include the following: <br />a lowercase letter, an
+            uppercase letter, a number, one of these special characters % ! @ #
+            $
+          </p>
         </div>
-    )
-}
+        <div className="registerDiv">
+          <label htmlFor="confirmPwd">Confirm Password:</label>
+          <div className="abraham">
+            <input
+              id="confirmPwd"
+              type={!togglePassword2 ? "password" : "text"}
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+              onFocus={() => setConfirmPasswordFocus(true)}
+              onBlur={() => setConfirmPasswordFocus(false)}
+            />
+            <FontAwesomeIcon
+              icon={faEye}
+              className="headerSearchIcon headerSearchIcon1"
+              onClick={tryAbraham2}
+            />
+          </div>
+          <p
+            className={
+              confirmPasswordFocus && !validConfirmPassword
+                ? "showInstructions"
+                : "hideInstructions"
+            }
+          >
+            Passwords must match.
+          </p>
+        </div>
 
-export default PasswordReset
+        <button
+          className="signUpButton"
+          disabled={!validPassword || !validConfirmPassword}
+        >
+          Submit new password
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default PasswordReset;
